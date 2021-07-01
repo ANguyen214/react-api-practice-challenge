da5bca4e2ef4275ea04f4ee10ac91e649c77adae
Alfredo08 <alfredosv08@gmail.com>
2021-07-01 09:07:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
           {/* Using The Open Brewery DB https://www.openbrewerydb.org/ build up a form that allows
               the user to type the name of a state of the United States and fetch to the api all breweries 
               listed in that state. The list of breweries must be saved in the state and displayed in the page.
-              Make use of the useEffect() hook to solve this challenge.*/}
+              */}
       </div>
     );
 }
